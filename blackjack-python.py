5a36fa2ec088561fe7993f596bafbd4722d6f65b
Bella <woo.bella@gmail.com>
2017-08-14 21:31:26 -0400

    Set up deck and to-do comments

---
+++ blackjack-python.py
@@ -0,0 +1,22 @@
+import random
+
+# Instantiate the deck
+suits = ('S','C','H','D')
+ranks = ('A','2','3','4','5','6','7','8','9','10','J','Q','K')
+# To-do: handle Ace 11 points
+card_value = ('A':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'J':10,'Q':10,'K':10)
+
+class Card:
+	pass
+
+class Deck:
+	pass
+
+class Hand:
+	pass
+
+# Game play
+# Create a Deck
+# Shuffle it
+# Give hands to players
+# Hitting and Standing
