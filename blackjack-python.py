f2c6d58be0b6bc53170cbdfa1ca0e6cddc4d5a07
Bella <woo.bella@gmail.com>
2017-08-16 22:11:23 -0400

    Player and Game classes

--- blackjack-python.py
+++ blackjack-python.py
@@ -1,11 +1,5 @@
 import random
 
-# Instantiate the deck
-suits = ('S','C','H','D')
-ranks = ('A','2','3','4','5','6','7','8','9','10','J','Q','K')
-# To-Do: handle Ace 11 points
-card_value = ('A':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'J':10,'Q':10,'K':10)
-
 class Card:
 	# Properties of a card
 	def __init__(self,suit,rank):
@@ -25,6 +19,11 @@ class Deck:
 	# Logic for dealing and shuffling
 	def __init__(self):
 		self.deck = []
+
+		suits = ('Spades','Clubs','Hearts','Diamonds')
+		ranks = ('Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King')
+		card_value = ('Ace':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'Jack':10,'Queen':10,'King':10)
+
 		# To-Do: Is there a product method equiv in Python?
 		for suit in suits:
 			for rank in ranks:
@@ -66,7 +65,7 @@ class Hand:
 			return self.value
 
 	def draw(self,hidden):
-		if hidden == True and playing == True:
+		if hidden == True and your_turn == True:
 			first_card = 1
 		else:
 			first_card = 0
@@ -74,25 +73,64 @@ class Hand:
 		for x in range(first_card,len(self.cards)):
 			self.cards[x].draw()
 
+class Player():
+	def __init__(self, player,purse=100):
+		self.player = player
+		self.purse = purse
+
+	def bet():
+		global bet
+		bet = 0
+		
+		print 'Enter bet (as whole integer)'
+
+		while bet == 0
+			bet_check = int(raw_input())
+
+			if bet_check >= 1 and bet_check <= purse:
+				bet = bet_check
+			else:
+				print 'Invalid bet, you only have ' + str(purse) + "left."
+
+	def hit():
+		pass
+
+	def stand():
+		pass
+
+class Game():
+
+	global move,your_turn,deck,player_hand,dealer_hand,purse,bet
+
+	def __init__(self):
+		self.dealer = Player()
+		self.player = Player()
+
+	def play():
+		deck = Deck()
+		deck.shuffle()
+
+		players.bet()
+
+		player_hand = Hand()
+		dealer_hand = Hand()
+
+		player_hand.add_card(deck.deal())
+		player_hand.add_card(deck.deal())
+
+		dealer_hand.add_card(deck.deal())
+		dealer_hand.add_card(deck.deal())
+
+		move = 'Hit or Stand? h/s'
+
+		if your_turn == True:
+			print 'Fold'
+			purse -= bet
+
+		your_turn = True
+		show_table()
+
+	def show_table():
+		pass
 
 
-# Game play
-def deal_cards():
-	deck = Deck()
-	deck.shuffle()
-
-	player_hand = Hand()
-	dealer_hand = Hand()
-
-	player_hand.add_card(deck.deal())
-	player_hand.add_card(deck.deal())
-
-	dealer_hand.add_card(deck.deal())
-	dealer_hand.add_card(deck.deal())
-
-# Hitting and Standing
-def hit():
-	pass
-
-def stand():
-	pass
