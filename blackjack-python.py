0c862eaad5910bb94e4064b0b476bd5a9603d95d
Bella <woo.bella@gmail.com>
2017-08-24 21:32:18 -0400

    hit, stand and winning

--- blackjack-python.py
+++ blackjack-python.py
@@ -20,9 +20,9 @@ class Deck:
 	def __init__(self):
 		self.deck = []
 
-		suits = ('Spades','Clubs','Hearts','Diamonds')
-		ranks = ('Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King')
-		card_value = ('Ace':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'Jack':10,'Queen':10,'King':10)
+		suits = ('S','C','H','D')
+		ranks = ('A','2','3','4','5','6','7','8','9','10','J','Q','K')
+		card_value = ('A':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'J':10,'Q':10,'K':10)
 
 		# To-Do: Is there a product method equiv in Python?
 		for suit in suits:
@@ -93,10 +93,55 @@ class Player():
 				print 'Invalid bet, you only have ' + str(purse) + "left."
 
 	def hit():
-		pass
+		if your_turn:
+			current_hand = player_hand.calc_hand_val()
+			if current_hand <= 21:
+				player_hand.add_card(deck.deal())
+
+				print "Player's hand is %s" %player_hand
+			elif current_hand > 21:
+				result = 'Bust'
+
+				purse -= bet
+				your_turn = False
+			else:
+				result = "Can't hit"
+
+			show_table()
+
 
 	def stand():
-		pass
+		if your_turn == False
+			player_score = player_hand.calc_hand_val()
+			dealer_score = dealer_hand.calc_hand_val()
+
+			if player_score > 0:
+				result = 'You have to hit'
+		else:
+			# Soft 17
+			while dealer_score < 17:
+				dealer_hand.add_card(deck.deal())
+			# Dealer bust
+			if dealer_score > 21:
+				result = 'Dealer bust. You win.'
+				purse += bet
+				your_turn = False
+			# Player win
+			elif dealer_score < player_score:
+				result = 'You beat the dealer. You win.'
+				purse += bet
+				your_turn = False
+			# Push
+			elif dealer_score == player_score:
+				result = 'Push'
+				your_turn == False
+			# Dealer wins
+			else:
+				result = 'Dealer wins.'
+				purse -= bet
+				your_turn = False
+
+		show_table()
 
 class Game():
 
@@ -131,6 +176,35 @@ class Game():
 		show_table()
 
 	def show_table():
-		pass
+		print ("Player's hand is: ")
+		player_hand.draw(hidden = False)
+		print "Player's hand total is: " + str(player_hand.calc_hand_val())
 
+		print ("Dealer's hand is: ")
+		dealer_hand.draw(hidden = True)
+		print "Dealer's hand total is: " + str(dealer_hand.calc_hand_val())
 
+		if your_turn == False:
+			print 'For a total of ' + str(dealer_hand.calc_hand_val())
+			print 'Chip total: ' = str(purse)
+		else:
+			print ' with another card hidden'
+
+		print result
+
+	def player_input():
+		chomp = raw_input().lower()
+
+		if chomp == 'h':
+			hit()
+		elif chomp == 's':
+			stand()
+		elif chomp == 'd':
+			deal()
+		elif chomp == 'q':
+			exit_game()
+		else:
+			print 'Invalid input. Enter h, s, d or q.'
+
+	def exit_game():
+		exit()
